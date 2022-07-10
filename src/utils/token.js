844aedc6bb9d42b7a2cfe8b1902ac4aea563a407
Lewiis <1819631759@qq.com>
2022-07-10 21:51:10 +0800

    fix 修改cookie的有效时间

--- src/utils/token.js
+++ src/utils/token.js
@@ -1,20 +1,20 @@
 export const _cookie = {
   setCookie: (name, value) => {
-    var Days = 30;
+    // var Days = 30;
     var exp = new Date();
-    exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+    // exp.setTime(exp.getTime() + Days * 24 * 60 * 60 * 1000);
+    exp.setTime(exp.getTime() + 24 * 60 * 60 * 1000);
     document.cookie =
-    name + "=" + encodeURIComponent(value)+ ";expires=" + exp.toGMTString();
+      name + "=" + encodeURIComponent(value) + ";expires=" + exp.toGMTString();
   },
   getCookie: (name) => {
     var arr,
       reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
-    if ((arr = document.cookie.match(reg))){
-    // console.log(unescape(arr[2]))
+    if ((arr = document.cookie.match(reg))) {
+      // console.log(unescape(arr[2]))
       // return unescape(decodeURIComponent(arr[2]));
       return decodeURIComponent(arr[2]);
-    }
-    else return null;
+    } else return null;
   },
   removeCookie: (name) => {
     var exp = new Date();
