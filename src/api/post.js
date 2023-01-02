0ceee27a9fa7c9778c1f39922d6b2b3d51124f05
Lewiis <1819631759@qq.com>
2023-01-03 00:39:51 +0800

    feat: ✨ 新增博客文章日志统计

--- src/api/post.js
+++ src/api/post.js
@@ -2,44 +2,53 @@ import request from "@/utils/request";
 
 // 获取文章列表
 export function fetchList(data) {
-  return request({
-    url: "/post/list",
-    method: "post",
-    data,
-  });
+	return request({
+		url: "/post/list",
+		method: "post",
+		data,
+	});
 }
 
 // 获取文章详情
 export function fetchArticle(params) {
-  return request({
-    url: "/post/article",
-    method: "get",
-    params: params,
-  });
+	return request({
+		url: "/post/article",
+		method: "get",
+		params: params,
+	});
 }
 
 // 文章热度+1
 export function addViewsCount(data) {
-  return request({
-    url: "/article/viewscount/add",
-    method: "post",
-    data,
-  });
+	return request({
+		url: "/article/viewscount/add",
+		method: "post",
+		data,
+	});
 }
 
 // 文章下载量+1
 export function addDownloadsCount(articleId) {
-  return request({
-    url: `/article/downloadscount/add/${articleId}`,
-    method: "put",
-  });
+	return request({
+		url: `/article/downloadscount/add/${articleId}`,
+		method: "put",
+	});
 }
 
 // 文章列表（筛选 分页）
 export function fetchArticleList(data) {
-  return request({
-    url: "/bms/fetchArticleList",
-    method: "post",
-    data,
-  });
+	return request({
+		url: "/bms/fetchArticleList",
+		method: "post",
+		data,
+	});
+}
+
+// 新增博客文章日志
+export function addArticleLog(data) {
+	return request({
+		url: `/article/log/add`,
+		method: "put",
+		data,
+	});
 }
--- vue.config.js
+++ vue.config.js
@@ -1,22 +1,22 @@
 module.exports = {
-  devServer: {
-    port: 8888,
-    // hot: true,
-    open: true,
-  },
-  lintOnSave: false,
-  configureWebpack: {
-    module: {
-      rules: [
-        // 配置读取 *.md 文件的规则
-        {
-          test: /\.md$/,
-          use: [
-            { loader: "html-loader" },
-            { loader: "markdown-loader", options: {} },
-          ],
-        },
-      ],
-    },
-  },
+	devServer: {
+		port: 8888,
+		// hot: true,
+		open: true,
+	},
+	lintOnSave: false,
+	configureWebpack: {
+		module: {
+			rules: [
+				// 配置读取 *.md 文件的规则
+				{
+					test: /\.md$/,
+					use: [
+						{ loader: "html-loader" },
+						{ loader: "markdown-loader", options: {} },
+					],
+				},
+			],
+		},
+	},
 };
