65a35da56a083ce99e3893eae8afa22542179374
qinguohui <qinguohui@nbicc.com>
2023-01-28 16:39:40 +0800

    fix: 🐛 重置md类型的博客下载逻辑

--- src/views/Articles.vue
+++ src/views/Articles.vue
@@ -133,12 +133,12 @@ export default {
 				this.$axios({
 					method: "get",
 					responseType: "blob", // 因为是流文件，所以要指定blob类型
-					url: `${process.env.VUE_APP_BASE_API}/getDoc?articleName=${this.article.content}`, // 自己的服务器，提供的一个word下载文件接口
+					url: `${process.env.VUE_APP_BASE_API}/getDoc?articleName=${article.content}`, // 自己的服务器，提供的一个word下载文件接口
 				}).then(({ data }) => {
 					const blob = new Blob([data]); // 把得到的结果用流对象转一下
 					var a = document.createElement("a"); //创建一个<a></a>标签
 					a.href = URL.createObjectURL(blob); // 将流文件写入a标签的href属性值
-					a.download = `${this.article.content}.docx`; //设置文件名
+					a.download = `${article.content}.docx`; //设置文件名
 					a.style.display = "none"; // 障眼法藏起来a标签
 					document.body.appendChild(a); // 将a标签追加到文档对象中
 					a.click(); // 模拟点击了a标签，会触发a标签的href的读取，浏览器就会自动下载了
@@ -146,7 +146,7 @@ export default {
 				});
 			}
 			// 调用接口增加文章下载数量
-			this.increaseDownloadsCount(this.article.id);
+			this.increaseDownloadsCount(article.id);
 			this.appendArticleLog(1);
 		},
 		// 文章下载量+1
