eb6adf34c1739115a2159777d10faa48f6a7e515
qinguohui <qinguohui@nbicc.com>
2023-04-21 15:54:24 +0800

    fix: ✨ 修改readme文件；删除无用的接口方法；

--- src/api/cdk.js
+++ src/api/cdk.js
@@ -1,14 +1,5 @@
 import request from "@/utils/request";
 
-// 保存访客日志
-export function saveVisitorInfo(data) {
-	return request({
-		url: "/visitorInfo/save",
-		method: "post",
-		data,
-	});
-}
-
 /**
  * 兑换cdk
  * @method put
